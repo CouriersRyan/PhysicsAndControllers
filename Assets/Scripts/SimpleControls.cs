58c62f73ee9a423a4055a8601bc2e4b069082097
Ryan Zhang <zhangryan09@gmail.com>
2022-04-01 12:23:02 -0400

    Added a Fox Player model with animations. Set up animations for movmeent

--- Assets/Scripts/SimpleControls.cs
+++ Assets/Scripts/SimpleControls.cs
@@ -1,49 +1,70 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SimpleControls : MonoBehaviour
 {
     private CharacterController _cc;
+    private Animator _anim;
     
     private Vector3 _moveVec;
-    [SerializeField] private bool _isJump;
-    [SerializeField] private bool isGrounded;
+    private bool _isJump;
+    private bool _isGrounded;
+
+    [SerializeField] private float gravity = 2f;
+    [SerializeField] private float moveSpd = 0.3f;
+    [SerializeField] private float jumpStrength = 1f;
+    
+    
+    private static readonly int Jump = Animator.StringToHash("OnJump");
+    private static readonly int Grounded = Animator.StringToHash("IsGrounded");
+    private static readonly int Velocity = Animator.StringToHash("Velocity");
 
     void Start()
     {
         _cc = GetComponent<CharacterController>();
+        _anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        isGrounded = _cc.isGrounded;
+        _isGrounded = _cc.isGrounded;
+        _anim.SetBool(Grounded, _isGrounded);
 
-        if (!_cc.isGrounded)
+        if (!_isGrounded)
         {
             if (_moveVec.y < 0) _isJump = false;
         } else
         {
+            _anim.SetFloat(Velocity, _moveVec.magnitude);
             if (!_isJump)
             {
                 _moveVec.y = 0;
             }
         }
         
-        _moveVec.y -= 4.5f * Time.deltaTime;
+        _moveVec.y -= gravity * Time.deltaTime;
         _cc.Move(_moveVec);
-
-        transform.rotation = Quaternion.LookRotation(new Vector3(_moveVec.x, 0, _moveVec.z));
+        
+        var forward = new Vector3(_moveVec.x, 0, _moveVec.z);
+        if(forward.magnitude != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(forward);
+        }
     }
     
 
     public void OnMove(InputValue input)
     {
+        var facing = Camera.main.transform.eulerAngles.y;
         Vector2 inputVec = input.Get<Vector2>();
+
+        var turnedVec = Quaternion.Euler(0, facing, 0) * inputVec;
         
-        var temp = new Vector3(inputVec.x, 0, inputVec.y) * 0.3f;
+        var temp = new Vector3(turnedVec.x, 0, turnedVec.y) * moveSpd;
 
         _moveVec.x = temp.x;
         _moveVec.z = temp.z;
@@ -53,8 +74,9 @@ public class SimpleControls : MonoBehaviour
     {
         if (_cc.isGrounded)
         {
-            _moveVec.y = 1f;
+            _moveVec.y = jumpStrength;
             _isJump = true;
+            _anim.SetTrigger(Jump);
         }
     }
 }
